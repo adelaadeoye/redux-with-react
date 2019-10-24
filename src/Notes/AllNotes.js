e6e94ad4ff327aff3862b00bd3b370b249dcdd13
adeoye adela <adeoye.adela@gmail.com>
2019-10-24 03:52:53 +0100

    App working

---
+++ src/Notes/AllNotes.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { removeNote } from '../redux/action';
+
+class AllNotes extends Component {
+
+  removeNote = (index) => {
+    this.props.removeNote(index);
+  }
+  
+  render() {
+    const notesItems = this.props.notes.map((note, index) =>
+      <li key={ index }>
+        <b>{ note.title }</b>
+        <button onClick={ () => this.removeNote(index) }>x</button>
+        <br />
+        <span>{ note.content }</span>
+      </li>
+    );
+
+    return (
+      <React.Fragment>
+        <h3>All Notes</h3>
+        
+        <ul>
+          { notesItems }
+        </ul>
+      </React.Fragment>
+    )
+  }
+  
+}
+
+const mapStateToProps = state => {
+  return {
+    notes: state.notes
+  };
+};
+
+
+
+export default connect(mapStateToProps, {removeNote})(AllNotes);
---
+++ src/redux/reducer/reducer.js
@@ -0,0 +1,27 @@
+import {ADD_NOTE,REMOVE_NOTE} from '../action'
+
+const initalState={
+    notes:[]
+}
+
+function rootReducer( state=initalState, action){
+
+    switch (action.type){
+        case ADD_NOTE:
+
+            return{
+               notes:[ ...state.notes, {
+                    title:action.title,
+                    content:action.content
+                }]
+
+            }
+case REMOVE_NOTE:
+    return {
+        notes: state.notes.filter((note, index)=>index !==action.id)
+    }
+            default: 
+            return state
+    }
+}
+export default rootReducer;
